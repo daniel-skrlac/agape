60ca8175aad9f46ef0cf65bb40ef9f1bc7a6685a
daniel-skrlac <221838399+daniel-skrlac@users.noreply.github.com>
2026-02-28 20:44:57 +0100

    Update generated.d.ts timestamp to reflect regeneration.

--- agape-mobile/src/models/generated.d.ts
+++ agape-mobile/src/models/generated.d.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.2.1263 on 2026-02-28 20:19:54.
+// Generated using typescript-generator version 3.2.1263 on 2026-02-28 20:31:40.
 
 export interface PagedResultDTO<T> {
     items: T[];
